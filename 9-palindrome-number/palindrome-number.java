3c1a2d1628e5c50c858be9425d8b905073c3205a
kartikey149 <kartikkesarwani149@gmail.com>
2025-12-10 18:36:36 +0530

    Time: 7 ms (16.55%) | Memory: 45.7 MB (51.69%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str=Integer.toString(x);
+    
+        char[] charArray =str.toCharArray();
+        // String str=Integer.toString(x);
+        int low=0;
+        int high=str.length()-1;
+        
+        while(low<=high){
+            char temp=charArray[low];
+            charArray[low]=charArray[high];
+            charArray[high]=temp;
+            
+            low++;
+            high--;
+            
+
+            
+        }
+        String revstr=new String(charArray);
+        if(revstr.equals(str)){
+            return true;
+        }
+
+        return false;
+        
+    
+
+        
+    }
+}
