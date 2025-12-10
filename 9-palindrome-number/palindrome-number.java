5e362a95a82f84a09dc07e3dbe203818e220f3ac
kartikey149 <kartikkesarwani149@gmail.com>
2025-12-10 19:00:20 +0530

    Time: 5 ms (72.73%) | Memory: 45.8 MB (41.77%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,25 +1,44 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String str=Integer.toString(x);
+        // String str=Integer.toString(x);
     
-        char[] charArray =str.toCharArray();
+        // char[] charArray =str.toCharArray();
         
-        int low=0;
-        int high=str.length()-1;
+        // int low=0;
+        // int high=str.length()-1;
         
-        while(low<=high){
-            char temp=charArray[low];
-            charArray[low]=charArray[high];
-            charArray[high]=temp;
+        // while(low<=high){
+        //     char temp=charArray[low];
+        //     charArray[low]=charArray[high];
+        //     charArray[high]=temp;
             
-            low++;
-            high--;
+        //     low++;
+        //     high--;
             
 
             
+        // }
+        // String revstr=new String(charArray);
+        // if(revstr.equals(str)){
+        //     return true;
+        // }
+        
+        if(x<0 || (x>0 && x%10==0)){
+            return false;
         }
-        String revstr=new String(charArray);
-        if(revstr.equals(str)){
+        int z=x;
+        int rev=0;
+        while(z!=0){
+            int a=z%10;
+
+            // System.out.println(a);
+            z=z/10;
+            rev=rev*10+a;
+            
+
+
+        }
+        if(x==rev){
             return true;
         }
 
