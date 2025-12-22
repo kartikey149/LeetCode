648ca95601ea691bc67589e978aa60d3b7944d42
kartikey149 <kartikkesarwani149@gmail.com>
2025-12-22 17:55:54 +0530

    Time: 1 ms (99.17%) | Memory: 47.1 MB (58.10%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int jump(int[] nums) {
+        if(nums.length<=1){
+            return 0;
+        }
+        int farthest=0;
+        int currentend=0;
+        int jump=0;
+        for(int i=0;i<nums.length;i++){
+            farthest=Math.max(farthest,i+nums[i]);
+            // farthest = Math.max(farthest, i + nums[i]);
+
+            if(i==currentend){
+                jump++;
+                currentend=farthest;
+                if(currentend>=nums.length-1){
+                    return jump;
+                }
+            }
+        }
+        
+
+            
+        return jump;
+
+        
+    }
+}
