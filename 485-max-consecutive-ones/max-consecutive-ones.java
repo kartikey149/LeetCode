bac500a50ef94acc1b165a2c10367ceb6046ee17
kartikey149 <kartikkesarwani149@gmail.com>
2025-12-28 00:24:58 +0530

    Time: 2 ms (97.68%) | Memory: 52.5 MB (35.51%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+        int ind=0;
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            count++;
+            
+            if(nums[i]!=1){
+            
+                count=0;
+                
+            }
+            if(count>max){
+                max=count;
+            }
+        }
+        return max;
+        
+    }
+}
