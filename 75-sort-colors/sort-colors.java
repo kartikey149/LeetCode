8cc16b3e4503242d08779b03d3b8bab526fab9a1
kartikey149 <kartikkesarwani149@gmail.com>
2025-12-30 16:15:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (20.65%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,19 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int[] freq=new int[3];
+        for(int i=0;i<nums.length;i++){
+            freq[nums[i]]++;
+        }
+        for(int i=1;i<freq.length;i++){
+            freq[i]+=freq[i-1];
+        }
+        int[] temp=new int[nums.length];
+        for(int i=nums.length-1;i>=0;i--){
+            temp[freq[nums[i]]-1]=nums[i];
+            freq[nums[i]]--;
+        }
+        for(int u=0;u<nums.length;u++){
+            nums[u]=temp[u];
+        }
     }
 }
