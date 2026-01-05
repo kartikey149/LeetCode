109e1a9e95deaa02a7c7791d7a26a719098a30b4
kartikey149 <kartikkesarwani149@gmail.com>
2026-01-05 23:24:17 +0530

    Time: 1 ms (99.76%) | Memory: 94.7 MB (14.09%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,37 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // int profit=0;
-        // int buy=0;
-        // for(int i=0;i<prices.length;i++){
-        //         int sell=0;
-        //     for(int j=i+1;j<prices.length;j++){
-        //         if(prices[i]>prices[j] || sell>prices[j]){
-        //             continue;
-        //         }
-            
-        //         if(sell<prices[j] && prices[i]<prices[j]){
-        //             int curprof=prices[j]-prices[i];
-
-        //             if(curprof>profit){
-        //                 profit=curprof;
-        //                 sell=prices[j];
-        //             }
-        //         }
-        //     }
-        // }
-        int minPrice = Integer.MAX_VALUE;
-        int maxProfit = 0;
-
-        for (int i = 0; i < prices.length; i++) {
-            // 1. Update the minimum price seen so far
-            if (prices[i] < minPrice) {
-                minPrice = prices[i];
-            } 
-            // 2. Calculate profit if we sold today and compare to maxProfit
-            else if (prices[i] - minPrice > maxProfit) {
-                maxProfit = prices[i] - minPrice;
+        int buy=Integer.MAX_VALUE;
+        int sell=0;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<buy){
+                buy=prices[i];
+            }
+            else if(prices[i]-buy>sell){
+                sell=prices[i]-buy;
             }
         }
-        return maxProfit;
+        return sell;
     }
 }
