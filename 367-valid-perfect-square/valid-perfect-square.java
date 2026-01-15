6e92f385fff4592692643760710a40b7fcfcc52e
kartikey149 <kartikkesarwani149@gmail.com>
2026-01-16 00:49:09 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (62.71%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -1,10 +1,25 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        for(long i=0;i*i<=num;i++){
-            if(i*i==num){
+        // for(long i=0;i*i<=num;i++){
+        //     if(i*i==num){
+        //         return true;
+        //     }
+
+        // }
+        long low=0;
+        long high=num;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            // mid*=mid;
+            if(mid*mid==num){
                 return true;
             }
+            else if(mid*mid<num){
+                low=mid+1;
+            }else if(mid*mid>num){
+                high=mid-1;
 
+            }
         }
         return false;
     }
