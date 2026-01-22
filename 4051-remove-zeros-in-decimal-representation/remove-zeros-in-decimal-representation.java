def7c875138347a15f635301ed1dbea07443d7c5
kartikey149 <kartikkesarwani149@gmail.com>
2026-01-23 00:18:10 +0530

    Time: 8 ms (5.29%) | Memory: 43.1 MB (22.88%) - LeetSync

--- 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.java
+++ 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.java
@@ -1,20 +1,52 @@
 class Solution {
     public long removeZeros(long n) {
-        // 1. Convert the long to a String to look at each digit
-        String st = String.valueOf(n);
-        StringBuilder sb = new StringBuilder();
         
-        // 2. Loop through each character (digit)
-        for (int i = 0; i < st.length(); i++) {
-            char digit = st.charAt(i);
-            // 3. Only append if the digit is not '0'
-            if (digit != '0') {
-                sb.append(digit);
+        String st=String.valueOf(n);
+
+        String s="";
+
+        long res=0;
+
+        for(int i=0;i<st.length();i++){
+
+            if(st.charAt(i)=='0'){
+
+                continue;
+
+            }else{
+                s+=st.charAt(i);
+
+
+
+
             }
+
         }
-        
-        // 4. Convert back to long (handle empty string case)
-        String res = sb.toString();
-        return res.isEmpty() ? 0 : Long.parseLong(res);
+
+        res=Long.parseLong(s);
+
+        return res;
+
+    
     }
 }
+// class Solution {
+//     public long removeZeros(long n) {
+//         // 1. Convert the long to a String to look at each digit
+//         String st = String.valueOf(n);
+//         StringBuilder sb = new StringBuilder();
+        
+//         // 2. Loop through each character (digit)
+//         for (int i = 0; i < st.length(); i++) {
+//             char digit = st.charAt(i);
+//             // 3. Only append if the digit is not '0'
+//             if (digit != '0') {
+//                 sb.append(digit);
+//             }
+//         }
+        
+//         // 4. Convert back to long (handle empty string case)
+//         String res = sb.toString();
+//         return res.isEmpty() ? 0 : Long.parseLong(res);
+//     }
+// }
