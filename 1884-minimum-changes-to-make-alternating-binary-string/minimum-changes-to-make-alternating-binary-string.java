5e1461b1b160c6a879df57f9e345be74c5fb172a
Kartikey Kesarwani <kartikkesarwani149@gmail.com>
2026-03-06 00:37:23 +0530

    Time: 5 ms (53.53%) | Memory: 44.1 MB (12.22%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int minOperations(String s) {
+        boolean[] ans = new boolean[s.length()];
+        boolean[] ans2 = new boolean[s.length()];
+
+        for (int i = 0; i < s.length(); i += 2) {
+            if (s.charAt(i) == '1') {
+                ans[i] = true;
+            }
+        }
+        for (int i = 1; i < s.length(); i += 2) {
+            if (s.charAt(i) == '0') {
+                ans[i] = true;
+            }
+        }
+
+        for (int i = 0; i < s.length(); i += 2) {
+            if (s.charAt(i) == '0') {
+                ans2[i] = true;
+            }
+        }
+        for (int i = 1; i < s.length(); i += 2) {
+            if (s.charAt(i) == '1') {
+                ans2[i] = true;
+            }
+        }
+
+        int co = 0;
+        int co2 = 0;
+        for (int i = 0; i < ans.length; i++) {
+            if (ans[i] == false) {
+                co++;
+            }
+        }
+        for (int i = 0; i < ans.length; i++) {
+            if (ans2[i] == false) {
+                co2++;
+            }
+        }
+
+
+        return co<co2?co:co2;
+
+    }
+}
