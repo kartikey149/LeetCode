185a1129c3ec8cf1eda662be0c5559cf35492c4d
Kartikey Kesarwani <kartikkesarwani149@gmail.com>
2026-03-17 13:23:06 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (89.59%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int trap(int[] height) {
+        int left=0;
+        int right=height.length-1;
+        int leftmax=0;
+        int rightmax=0;
+        int sum=0;
+
+        while(left<right){
+            if(height[left]<height[right]){
+                if(height[left]>leftmax){
+                    leftmax=height[left];
+                }else{
+                    sum+=leftmax-height[left];
+                }
+            left++;
+            }
+            else{
+                if(height[right]>rightmax){
+                    rightmax=height[right];
+                }
+                else{
+                    sum+=rightmax-height[right];
+                }
+                right--;
+            }
+        }
+        return sum;
+
+        
+    }
+}
