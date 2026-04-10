5e9dfce0038672144037f5152c7082bc06ac2ae8
Kartikey Kesarwani <kartikkesarwani149@gmail.com>
2026-04-11 00:16:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (89.17%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -4,14 +4,14 @@ class Solution {
         // if(n<=3)return n;
         // return climbStairs(n-1) + climbStairs(n-2);
 
-        if(n<3) return n;
-        int a=1,b=2;
+        if(n<=3) return n;
+        int a=2,b=3;
         int ans=0;
-        for(int i=3;i<=n;i++){
+        for(int i=4;i<=n;i++){
             ans=a+b;
             a=b;
             b=ans;
         }
-        return ans;
+        return b;
     }
 }
