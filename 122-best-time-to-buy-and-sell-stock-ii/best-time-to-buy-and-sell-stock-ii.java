db606e39a70bb00d0613011c0966666f799ac598
Kartikey Kesarwani <kartikkesarwani149@gmail.com>
2026-05-12 00:06:26 +0530

    Time: 97 ms (4.88%) | Memory: 49.6 MB (7.69%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,9 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int profit=0;
-        for(int i=1;i<prices.length;i++){
-            if(prices[i]>prices[i-1]){
-                profit+=prices[i]-prices[i-1];
+        int buy=Integer.MAX_VALUE;
+        
+        for(int i=0;i<prices.length;i++){
+            buy=Math.min(buy,prices[i]);
+            System.out.println(buy+"buy");
+            if(i>0 && prices[i]>prices[i-1]){
+
+                profit+=prices[i]-buy;
+            System.out.println(profit+"sell");
+                buy=prices[i];
+            // System.out.println(buy+"buy");
             }
         }
         return profit;
