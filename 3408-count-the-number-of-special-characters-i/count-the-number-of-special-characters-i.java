f40de9af3aaab736bb0a97ae044e7d940cafed88
Kartikey Kesarwani <kartikkesarwani149@gmail.com>
2026-05-26 17:57:35 +0530

    Time: 3 ms (31.81%) | Memory: 44 MB (33.52%) - LeetSync

---
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        
+
+        // HashMap<Character, Interger> map = new HashMap<>();
+        // HashMap<Character, Interger> map2 = new HashMap<>();
+
+
+        // for(int i=65;i<=90;i++){
+        //     map.put((char)i,i);
+        // }
+
+        
+        // for(int i=97;i<=122;i++){
+        //     map2.put((char)i,i);
+        // }
+        // StringBuilder sb=new StringBuilder(word)
+        // for(char c:sb){
+        //     if(map.containsKey(c)   ){
+        //         int index = sb.indexOf(String.valueOf(c));
+        //         int index2 = sb.indexOf(String.valueOf(c));
+
+
+
+        //         sb.deleteCharAt(index);
+        //         ans++;
+        //     }
+
+        // }
+
+        Set<Integer> map1 = new HashSet<>();
+        Set<Integer> map2 = new HashSet<>();
+
+        for(int i=0;i<word.length();i++){
+
+            int j = (int)word.charAt(i);
+
+            if(j>91)
+                map1.add(j);
+            else
+                map2.add(32+j);
+
+        }
+
+        map1.retainAll(map2);
+
+        return map1.size();
+
+
+        
+
+    }
+}
