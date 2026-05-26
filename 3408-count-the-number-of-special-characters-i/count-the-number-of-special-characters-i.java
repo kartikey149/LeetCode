0ec6a27d72cdbe75c26a92c530402458554fbc24
Kartikey Kesarwani <kartikkesarwani149@gmail.com>
2026-05-26 18:12:19 +0530

    Time: 2 ms (48.76%) | Memory: 44.2 MB (10.29%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -28,23 +28,54 @@ class Solution {
 
         // }
 
-        Set<Integer> map1 = new HashSet<>();
-        Set<Integer> map2 = new HashSet<>();
+        // Set<Integer> map1 = new HashSet<>();
+        // Set<Integer> map2 = new HashSet<>();
 
-        for(int i=0;i<word.length();i++){
+        // for(int i=0;i<word.length();i++){
 
-            int j = (int)word.charAt(i);
+        //     int j = (int)word.charAt(i);
 
-            if(j>91)
-                map1.add(j);
-            else
-                map2.add(32+j);
+        //     if(j>91)
+        //         map1.add(j);
+        //     else
+        //         map2.add(32+j);
 
+        // }
+
+        // map1.retainAll(map2);
+
+        // return map1.size();
+
+
+
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char c:word.toCharArray()){
+            map.put(c,(int)c);
         }
+        int ans=0;
 
-        map1.retainAll(map2);
+        // StringBuilder sb =new StringBuilder(word);
+        for(char c:word.toCharArray()){
+            // int index=sb.indexOf(String.valueOf(c));
+            if((int)c>91){
+                if(map.containsKey((char)(c-32))){
+                    ans++;
+                    map.remove(c);
+                    map.remove((char)(c-32));
 
-        return map1.size();
+                }
+            }
+            else if((int)c>=65 && (int )c<=90){
+                if(map.containsKey((char)(c+32))){
+                    ans++;
+                    map.remove(c);
+                    map.remove((char)(c+32));
+                }
+
+
+            }
+        }
+        return ans;
 
 
         
