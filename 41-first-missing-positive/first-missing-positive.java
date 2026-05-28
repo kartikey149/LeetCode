a18a985064fb0808d50a724e3b32d522e6b53f13
Kartikey Kesarwani <kartikkesarwani149@gmail.com>
2026-05-28 15:09:02 +0530

    Time: 1 ms (100.00%) | Memory: 70.9 MB (91.51%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,29 +1,22 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n=nums.length;
-        
-        // int[] hashTable = new int[maxElement + 1];
-        boolean[] hashTable=new boolean[n+1];
-
-    
-        for (int num : nums) {
-            if(num>0 && num<=n){
-                hashTable[num]=true;
-
-            }
-        }
-
-        
-        for (int i = 1; i < hashTable.length; i++) {
-            // if (hashTable[i] > 0) {
-            //     System.out.println(hashTable[i]);
-            // }
-            if(!hashTable[i]){
-                return i;
+        int i=0;
+        while(i<nums.length){
+            int co=nums[i]-1;
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[co]) {
                 
+                int temp = nums[i];
+                nums[i] = nums[co];
+                nums[co] = temp;
+            }
+            else i++;
+        }
+        int ans=0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                return j + 1;
             }
         }
-        return n+1;
-        
+        return nums.length+1;
     }
 }
