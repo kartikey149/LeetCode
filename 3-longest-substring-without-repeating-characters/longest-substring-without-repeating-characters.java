4077ce2fb2ebba44852ec56e79981ccf9094401c
Kartikey Kesarwani <kartikkesarwani149@gmail.com>
2026-08-08 17:05:44 +0530

    Time: 55 ms (16.54%) | Memory: 47.7 MB (14.43%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,22 +1,50 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        // HashMap<Character,Integer> map=new HashMap<>();
+        // int i=0;
+        // int co=0;
+        // int anns=0;
+        // while(i<s.length()){
+        //     if(!map.containsKey(s.charAt(i))){
+        //         map.put(s.charAt(i),i);
+        //         i++;
+        //         co++;
+        //         anns=Math.max(co,anns);
+        //     }
+        //     else{
+        //         i=map.get(s.charAt(i))+1;
+        //         map.clear();
+        //         co=0;
+        //     }
+        // }
+        // return anns;
+
+
+        // HashMap<Character,Integer> map=new HashMap<>();
+        // // int i=0;
+        // int ans=0,j=0;
+        // for(int i=0;i<s.length();i++){
+        //     char c=s.charAt(i);
+        //     map.put(c,map.getOrDefault(c,0)+1);
+        //     if(map.containsKey(c) && map.get(c)>1){
+        //         map.put(c,map.getOrDefault(c,0)+1);
+        //         j=i;
+        //     }
+        //     ans=Math.max(ans,map.size());
+        // }
+        // return ans;
         HashMap<Character,Integer> map=new HashMap<>();
-        int i=0;
-        int co=0;
-        int anns=0;
-        while(i<s.length()){
-            if(!map.containsKey(s.charAt(i))){
-                map.put(s.charAt(i),i);
-                i++;
-                co++;
-                anns=Math.max(co,anns);
+        int ans=0,l=0;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(map.containsKey(c) && map.get(c)>=l){
+                l=map.get(c)+1;
+                map.put(c,i);
             }
-            else{
-                i=map.get(s.charAt(i))+1;
-                map.clear();
-                co=0;
-            }
+            map.put(c,i);
+            ans=Math.max(i-l+1,ans);
         }
-        return anns;
+        return ans;
+        
     }
 }
