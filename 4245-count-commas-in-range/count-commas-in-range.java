462eda0b0fa62d3174a1582330ef503fd815e5d7
Kartikey Kesarwani <kartikkesarwani149@gmail.com>
2026-09-08 22:17:46 +0530

    Time: 1 ms (83.65%) | Memory: 41.9 MB (99.56%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countCommas(int n) {
+        int count=0;
+        if(n<1000) return 0;
+        else if(n<1000000) {
+            count+=n-999;
+        }
+        return count;
+
+    }
+}
