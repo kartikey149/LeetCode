42a0477cc4e5d151af603289ce6235a007eb80ec
Kartikey Kesarwani <kartikkesarwani149@gmail.com>
2026-09-10 23:33:43 +0530

    Time: 3 ms (2.76%) | Memory: 44.7 MB (51.28%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,39 +1,32 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        // int last=nums[nums.length
         int i=nums.length-2;
-       
-        // int i = nums.length - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
+        // int j=nums.length-1;
+        while(i>=0 && nums[i]>=nums[i+1]){
             i--;
         }
         if(i>=0){
+            System.out.print("enter"+i);
             int j=nums.length-1;
-        while(j>i && nums[j]<= nums[i]){
-            // min=Math.min(min,nums[j]);/
-            j--;
+            while(j>i && nums[j]<=nums[i]){
+                j--;
+            }
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
         }
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-        }
+        rev(nums,i+1,nums.length-1);
 
         
-        
-            reve(nums,i+1,nums.length);
-        
 
-        
     }
-    public static void reve(int[] nums ,int start,int end){
-        int a=start;
-        int b=end-1;
-        while(a<b){
-            int temp=nums[a];
-                nums[a]=nums[b];
-                nums[b]=temp;
-                a++;
-                b--;
+    static void rev(int[] nums,int i,int j){
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
         }
     }
 }
